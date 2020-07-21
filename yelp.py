eb82cab2dd6f82d208bf048fc5a7bb697a45bf24
Andrea Czarnecki <ancheetz@me.com>
2020-07-21 16:22:50 -0400

    defined functions

--- yelp.py
+++ yelp.py
@@ -13,27 +13,27 @@ page = response.text
 #store the webpage in soup
 soup = bs(page, 'lxml')
 
-#find reviewer name
-reviewer = [name.text for name in soup.find_all('div', 'a', 'href', class_='user-passport-info')]
-print(reviewer[0])
-print(type(reviewer))
-#convert list to json string, formatted with indents
-name = (json.dumps(reviewer, indent=4))
-#print(name)
+def yelp_reviewer():
+    #find reviewer name
+    reviewer = [name.text for name in soup.find_all('div', 'a', 'href', class_='user-passport-info')]
+    #convert list to json string, formatted with indents
+    name = (json.dumps(reviewer, indent=4))
+    print(name)
 
-#find review
-review = [rev.text for rev in soup.find_all('span', lang='en')]
-print(review[0])
-#convert list to json string, formatted with indents
-rest_rev = (json.dumps(review, indent=6))
-#print(rest_rev)
+def yelp_review():
+    #find review
+    review = [rev.text for rev in soup.find_all('span', lang='en')]
+    #convert list to json string, formatted with indents
+    rest_rev = (json.dumps(review, indent=6))
+    print(rest_rev)
 
-#find date of review
-date_vis = [d.text for d in soup.find_all('span', class_='text-color--mid__373c0__jCeOG')]
-#print(date_vis)
-#convert list tp json string,
-date_rev = (json.dumps(date_vis, indent=4))
-print(date_rev)
+def yelp_rev_date():
+    #find date of review
+    date_vis = [d.text for d in soup.find_all('span', class_='text-color--mid__373c0__jCeOG')]
+    #convert list tp json string,
+    date_rev = (json.dumps(date_vis, indent=4))
+    print(date_rev)
 
-
-
+yelp_reviewer()
+yelp_review()
+yelp_rev_date()
